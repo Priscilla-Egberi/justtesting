ee4399662e6397bd9f9609958823abe516e61247
Priscilla <priscillaegberi@gmail.com>
2022-09-14 20:23:22 +0000

    Update 3 files

--- src/Jack.jsx
+++ src/Jack.jsx
@@ -4,7 +4,8 @@ function Card() {
   return (
     <div>
       <button className="btn btn-primary">button</button>
-      <div className="card">I think this is my breakthrough</div>
+      <div className="card d-inline-block" style={{width:"18rem"}}>I think this is my breakthrough</div>
+      <div className="card d-inline-block" style={{width:"18rem"}}>I think this is my breakthrough</div>
     </div>
   );
 }
