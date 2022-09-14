466006bcd369b118e0306e3f9c2444d9f7777b54
Priscilla-Egberi <100158570+Priscilla-Egberi@users.noreply.github.com>
2022-09-14 20:04:52 +0100

    Initial commit

---
+++ src/Jack.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div>
+      <button className="btn btn-primary">button</button>
+      <div className="card">I think this is my breakthrough</div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
